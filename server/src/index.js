11fc153dd96b87389e80712581fe6eff8653176f
Amul <amulgairola100@gmail.com>
2023-12-08 11:06:54 +0530

    inital commit

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import {userRouter} from "./routes/User.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/auth", userRouter);
+
+mongoose.connect("mongodb+srv://amul48:amulge48100@cluster0.urjoqxs.mongodb.net/Cluster0?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.listen(3001, () => console.log("Server chal raha reh"));
+
